479d57f2664042a1ee7fd158f1b250d68f0cee27
Timothy A. DeWees <timothy.dewees@sherwin.com>
2024-02-08 08:49:30 -0500

    Removed unneeded import

--- src/main/java/com/tdtech/scorecard/api/round/controller/RoundController.java
+++ src/main/java/com/tdtech/scorecard/api/round/controller/RoundController.java
@@ -5,11 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tdtech.scorecard.api.round.entity.RoundRequest;
 import com.tdtech.scorecard.api.round.entity.RoundResponse;
 import com.tdtech.scorecard.api.round.service.RoundService;
-
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
