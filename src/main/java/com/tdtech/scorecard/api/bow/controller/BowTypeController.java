e7744757653cd1ce06da60665f943d4ac78591b1
Timothy A. DeWees <timothy.dewees@sherwin.com>
2024-02-09 08:52:04 -0500

    More unused imports

--- src/main/java/com/tdtech/scorecard/api/bow/controller/BowTypeController.java
+++ src/main/java/com/tdtech/scorecard/api/bow/controller/BowTypeController.java
@@ -1,16 +1,13 @@
 package com.tdtech.scorecard.api.bow.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-
-import com.tdtech.scorecard.api.bow.entity.BowTypeResponse;
-import com.tdtech.scorecard.api.bow.service.BowTypeService;
-
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
+
+import com.tdtech.scorecard.api.bow.entity.BowTypeResponse;
+import com.tdtech.scorecard.api.bow.service.BowTypeService;
 
 @RestController
 @RequestMapping("/scorecard-api/v1.0")
